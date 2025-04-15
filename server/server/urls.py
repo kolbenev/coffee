e6750885b6fa8311a8118ad323bb31c495e627c0
Dmitry Kolbenev <kolbenev.d.s@gmail.com>
2025-04-15 23:41:25 +0300

    change settings

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -9,3 +12,6 @@ urlpatterns = [
     path('api/', include('prices.urls')),
     path('api/callback/', include('callback.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

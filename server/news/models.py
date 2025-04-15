78366a653532e234aeead5534d09cc26231d69e4
Dmitry Kolbenev <kolbenev.d.s@gmail.com>
2025-04-15 23:43:56 +0300

    fix news

--- server/news/models.py
+++ server/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class News(models.Model):
     text = models.TextField()
-    photo = models.ImageField(upload_to='../images/news_photos/')
+    photo = models.ImageField(upload_to='images/news_photos/')
     date = models.DateField()
 
     def __str__(self):

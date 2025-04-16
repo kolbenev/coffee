9d2e77ee62e5eb3713be46e956b4202525423f02
Gfhjkmfl@1@1 <kostechkina@gmail.com>
2025-04-16 16:25:25 +0400

    fix menu

--- frontend/src/widgets/Header/ui/Header.jsx
+++ frontend/src/widgets/Header/ui/Header.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "@/shared/ui/Logo/Logo";
 import Container from "@/shared/ui/Container/Container";
 import Menu from "./Menu";
 import MenuButton from "./MenuButton";
 
 import cls from "./Header.module.css";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const [activeMenu, setActiveMenu] = useState(false);
+  const { pathname } = useLocation();
 
   const handleClick = () => {
     setActiveMenu((b) => !b);
-    document.querySelector(".app").classList.toggle("overflow");
+    document.querySelector(".app")?.classList.toggle("overflow");
   };
+
+  useEffect(() => {
+    setActiveMenu(false);
+    document.querySelector(".app")?.classList.remove("overflow");
+  }, [pathname]);
   return (
     <header className={cls.header}>
       <Container className={cls.inner}>

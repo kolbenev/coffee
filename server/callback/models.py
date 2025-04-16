2e8890a404829e4e84d75c48c92b249070dd4d71
Dmitry Kolbenev <kolbenev.d.s@gmail.com>
2025-04-16 03:52:33 +0300

    change callback

--- server/callback/models.py
+++ server/callback/models.py
@@ -5,6 +5,7 @@ class CallbackRequest(models.Model):
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=20)
     comment = models.TextField(blank=True, null=True)
+    preferred_time = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- server/callback/serializers.py
+++ server/callback/serializers.py
@@ -6,11 +6,11 @@ from .models import CallbackRequest
 class CallbackRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = CallbackRequest
-        fields = ['id', 'name', 'phone', 'comment', 'email', 'created_at']
+        fields = ['name', 'phone', 'comment', 'email', 'preferred_time']
         read_only_fields = ['id', 'created_at']
 
     def validate_phone(self, value):
-        pattern = r'^(\+7|8)\d{10}$'
+        pattern = r'^(\+7|8|7)\d{10}$'
         cleaned = re.sub(r'[^\d+]', '', value)
         if not re.match(pattern, cleaned):
             raise serializers.ValidationError("Incorrect phone number")
